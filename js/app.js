72e58f6fdd77d802d644d0ae38fbd0f441a8844a
FernandoGS65 <fernandogs65@users.noreply.github.com>
2026-06-20 22:18:57 +0200

    Rediseño header/footer: icon nav, selectores temp/comp, footer minimal

--- js/app.js
+++ js/app.js
@@ -158,6 +158,34 @@ var APP = (function() {
         }
     }
 
+    /* ===== NAV ICONS ===== */
+    function renderNavIcons() {
+        var container = document.getElementById('nav-icons');
+        if (!container) return;
+        var pagina = window.location.pathname.split('/').pop() || 'index.html';
+
+        var icons = [
+            { href: 'index.html', page: 'index.html', label: 'Inicio', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/></svg>' },
+            { href: 'equipos.html', page: 'equipos.html', label: 'Equipos', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/></svg>' },
+            { href: 'resultados.html', page: 'resultados.html', label: 'Resultados', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/></svg>' },
+            { href: 'clasificaciones.html', page: 'clasificaciones.html', label: 'Clasif.', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>' }
+        ];
+
+        var html = '';
+        icons.forEach(function(icon) {
+            var active = pagina === icon.page ? ' nav-icon--active' : '';
+            html += '<a href="' + icon.href + '" class="nav-icon' + active + '">' + icon.svg + '<span>' + icon.label + '</span></a>';
+        });
+        container.innerHTML = html;
+    }
+
+    /* Override init to also render nav icons */
+    var origRender = renderNavSelectors;
+    renderNavSelectors = function() {
+        origRender();
+        renderNavIcons();
+    };
+
     /* ===== CHANGE LISTENERS ===== */
     function onChange(fn) {
         listeners.push(fn);
