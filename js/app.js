ac75f30b3e4a24ac3607b8a91b3ce5f2ad3be08a
FernandoGS65 <fernandogs65@users.noreply.github.com>
2026-06-27 00:03:31 +0200

    feat: añade Estadísticas al menú de navegación

--- js/app.js
+++ js/app.js
@@ -171,6 +171,7 @@ var APP = (function() {
             { href: 'equipos.html', page: 'equipos.html', label: 'Equipos', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/></svg>' },
             { href: 'resultados.html', page: 'resultados.html', label: 'Resultados', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/></svg>' },
             { href: 'clasificaciones.html', page: 'clasificaciones.html', label: 'Clasif.', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>' },
+            { href: 'estadisticas.html', page: 'estadisticas.html', label: 'Estad.', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/></svg>' },
             { href: 'arbitros.html', page: 'arbitros.html', label: 'Arbitros', svg: '<svg class="nav-icon-svg" viewBox="0 0 24 24"><path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>' }
         ];
 
